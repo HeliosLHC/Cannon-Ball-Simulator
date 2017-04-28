3db89f8c557bbe67f03417d5f170573976a7cc8f
HeliosLHC <groudon2224@gmail.com>
2017-04-28 06:03:36 -0400

    Fized Stuff
    53

--- main.js
+++ main.js
@@ -1,87 +1,121 @@
 
 // Global Variables
 	// Static Variables
-	var ballX = $("circle").attr('cx');
-	var ballY = $("circle").attr('cy');
-	var velocityInput = velocityInput =  parseInt($("#velocityInput").val());
-	var angleDeg = angleDeg = parseInt($("#angleInput").val());
+	var ballX = parseInt($("circle").attr('cx'));
+	var ballY = parseInt($("circle").attr('cy'));
+	var velocityInput;
+	var angleDeg;
 
 	// Dynamic Variables	
 	var currentVelocity;
-	var acceleration = 9.81;
+	var acceleration = -9.81;
 	var simActive = false;
-	var time = 0;
-	var dateObject = new Date();
-	var initTime;
-	var currTime;
-	var timeDelta;
 	var velocityX;
 	var velocityY;
 	var angleRad;
+	var ballRadius;
+		// Time Variable
+			var time;
+			var startTime;
+			var currTime = 0;
+			var timeDelta;
+
 	// DEBUG
 		// gSA
 		var gameStateArray;
 		// iA
 		var inputArray;
 // UI
-$("#start-btn").on('click', function(event) {
-	event.preventDefault();
-	/* Act on the event */
-	simActive = true;
+	// Initiate Simulator State
+		$(document).ready(function() {
+			// Canvas Resize
+			windowHeight = window.innerHeight;
+			$("#canvas").css('height', windowHeight);
+			$("svg").css('height', windowHeight * 0.8);
+			// Set Ball Position
+			$("circle").attr({
+				cy: windowHeight * 0.75
+			});
+		});
 
-	// Set InitTime 
-	initTime = dateObject.getTime();
-	
-	// Set Velocity
-	velocityX = Math.cos(angleRad); 
-	velocityY = Math.sin(angleRad);
+	// Reset	
+		$("#reset-btn").on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			$("#velocityInput,#angleInput,#ballRadius").val("");
+			console.log("Test")
+		});	
 
-	// Set Angle
-	angleRad = radToDeg(angleDeg);
+	// Button Event
+	$("#start-btn").on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			simActive = true;
+			
+		 	// Set Ball Radius
+		 	ballRadius = parseInt($("#ballRadius").val())
 
-	// Initiate Render Loop
-	setInterval(render,10);
-	// DEBUG
-	console.log("Sim Started " + "SimActive: " + simActive);
-// Input
-	// Velocity Validation
+			// Set Velocity
+			velocityInput = parseInt($("#velocityInput").val())
+			velocityX = Math.cos(angleRad); 
+			velocityY = Math.sin(angleRad);
 
-	// Angle Validation
+			// Set Angle
+			angleDeg = parseInt($("#angleInput").val());
+			angleRad = radToDeg(angleDeg);
 
-		
-		
+			// Initiate Render Loop
+			setInterval(render,1);
+			// DEBUG
+				console.log("Sim Started " + "SimActive: " + simActive);
+
+				// Start DEBUG loop
+				setInterval(debug,1000)
+
+		// Input
+			// Velocity Validation
+
+			// Angle Validation
+		}
+	)
 
 // Degree to Radians Calculator
 	function radToDeg(angle) {
-		return angle * (180 / Math.PI);
+		return angle / (180 / Math.PI);
 	}
 // Render
 	function render() {
 		// Update Time
-		dateObject = new Date();
-		currTime = dateObject.getTime();
-		timeDelta = Math.round((currTime - initTime)*0.001);
+			time = new Date().getTime();
+			currTime = new Date().getTime();
+			timeDelta = 0.001 * (currTime - startTime);
 		// Set Velocities
 		velocityX = Math.cos(angleRad)*velocityInput;
 		velocityY = Math.sin(angleRad)*velocityInput;
-			// DEBUG
-			console.log("Velocities are")
-				// Set DEBUG variables
-				gameStateArray = {"simActive":simActive,"Time":timeDelta}
-				inputArray = {"X Velocity":velocityX,"Y Velocity":velocityY}
-				// Output DEBUG variables
-				console.log("gameStateArray");
-				console.log("inputArray")
+			
 		// Distance 
-		ballX=velocityX*timeDelta;
-		ballY=velocityY*timeDelta + 0.5*acceleration*(Math.pow(timeDelta,2));
+		ballX = velocityX * timeDelta;
+		ballY =  (windowHeight * 0.75) - (velocityY*timeDelta + 0.5*acceleration*(Math.pow(timeDelta,2)));
+		
 		// Apply Attributes
-		$("circle").attr({
-		cx: ballX,
-		cy: ballY,
-		r: "10"
-		});
+			$("circle").attr({
+			cx: ballX + ballRadius,
+			cy: ballY ,
+			r: ballRadius
+			});
 		}
-		}
-	)
+		
 // Collision
+
+// DEBUG
+	function debug() {
+		// console.log("Velocities are")
+		// 		// Set DEBUG variables
+		// 		gameStateArray = {"simActive":simActive,"Time":currTime}
+		// 		inputArray = {"X Velocity":velocityX,"Y Velocity":velocityY}
+		// 		// Output DEBUG variables
+		// 		console.log("gameStateArray");
+		// 		console.log("inputArray")
+		console.log(timeDelta)
+	}
+			
